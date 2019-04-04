a3a21983b8b9598f671a8a998a65b4d9b9317928
Mariusz Słapek <45197338+mslapek1@users.noreply.github.com>
2019-04-04 16:16:16 +0200

    Update test.py

--- Laboratoria 1/catkin_ws/src/lab1/src/test.py
+++ Laboratoria 1/catkin_ws/src/lab1/src/test.py
@@ -16,8 +16,10 @@ def move():
 	# inicjacja wezla
 	rospy.init_node('lab1')
     
+	# ustawienie czestotliwosci publikowania
 	rate = rospy.Rate(10) # 10hz
 
+	# pobranie parametrow z serwera parametrow - klawisze sterowania
 	FORWARD = rospy.get_param("forward")
 	BACKWARD = rospy.get_param("backward")
 	TURNLEFT = rospy.get_param("turnLeft")
@@ -26,19 +28,26 @@ def move():
 	twist = Twist()
 
 	while not rospy.is_shutdown():
+
+		# pobranie wartosci wcisnietego klawisza
 		letter = getchar()
 
+		# pobranie aktualnych wartosci predkosci liniowej i katowej z serwera parametrow
+		LINVELOCITY = rospy.get_param("linVelocity")
+		ANGVELOCITY = rospy.get_param("angVelocity")
+
+		# ustawienie odpowiednich predkosci w zaleznosci od wartosci wcisnietego klawisza
 		if letter == FORWARD:
-			twist.linear.x = 1.0
+			twist.linear.x = LINVELOCITY
 			twist.angular.z = 0.0
 		elif letter == TURNLEFT:
 			twist.linear.x  = 0.0
-			twist.angular.z = 1.0
+			twist.angular.z = ANGVELOCITY
 		elif letter == TURNRIGHT:
 			twist.linear.x  = 0.0
-			twist.angular.z = -1.0
+			twist.angular.z = -ANGVELOCITY
 		elif letter == BACKWARD:
-			twist.linear.x  = -1.0
+			twist.linear.x  = -LINVELOCITY
 			twist.angular.z = 0.0
 
 		# publikowanie wiadomosci
