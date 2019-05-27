3bd26cadfce3b007d722289179a955cef9b6b82d
Mariusz Słapek <45197338+mslapek1@users.noreply.github.com>
2019-05-27 12:38:50 +0200

    Update ocmd.py
    kdl

--- catkin_ws/src/lab2/src/ocmd.py
+++ catkin_ws/src/lab2/src/ocmd.py
@@ -10,7 +10,12 @@ from tf.transformations import *
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import PoseStamped
 import math
-
+import PyKDL as kdl
+from tf.transformations import *
+kat=0.2
+xaxis, yaxis, zaxis = (1, 0, 0), (0, 1, 0), (0, 0, 1)
+rotation = kdl.Rotation(1, 0, 0, 0, math.cos(kat), -math.sin(kat), 0, math.sin(kat), math.cos(kat))
+translation = kdl.Vector(0,0,0.5)
 
 time = 10
 def kwadrat(radius):
@@ -28,7 +33,13 @@ def elipsa(x, y):
     for i in range(n):
 	xi=x*math.cos(math.pi*2.0*i/n)
 	yi=y*math.sin(math.pi*2.0*i/n)
-        trajektoria(xi, yi, 0, 0.2)       
+
+	zi = 0
+	frame = kdl.Frame( rotation, translation)
+	vector = kdl.Vector(xi,yi,zi)
+	end=frame*vector
+	
+        trajektoria(end.x(), end.y(),end.z(), 0.2)       
 
 def trajektoria(x, y, z, time):
     rospy.wait_for_service('oint')
@@ -43,4 +54,5 @@ def usage():
     return "%s [x y]"%sys.argv[0]
 
 if __name__ == "__main__":
-    elipsa(2.5,2)
+    elipsa(2.2,1.8)
+    #kwadrat(1.7)
